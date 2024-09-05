23da776760273cd65336d76c21db815b43d29040
dhruv0767 <69031470+dhruv0767@users.noreply.github.com>
2024-09-05 15:34:05 +0000

    v3

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,16 +1,30 @@
 import streamlit as st
 import lab1
 import lab2
+import lab3
 
-# Title of the app
-st.title("Lab Navigator")
+# Title for the main page
+st.title('Simple Streamlit App')
 
-# Create a simple navigation using radio buttons or a selectbox
-selected_lab = st.radio("Select a Lab", ['Lab 1', 'Lab 2'])
+# Sidebar selection
+st.sidebar.title("Navigation")
+selection = st.sidebar.radio("Go to", ["Home", "Lab 1", "Lab 2", "Lab 3"])
 
-# Display the corresponding lab based on the selection
-if selected_lab == 'Lab 1':
-    lab1.show_lab()
+# Home Page
+if selection == "Home":
+    st.write("""
+    ## Welcome to the Dhruv's Streamlit Labs App
+    Use the sidebar to navigate to different labs.
+    """)
 
-elif selected_lab == 'Lab 2':
-    lab2.show_lab()
+# Lab 1 Page
+elif selection == "Lab 1":
+    lab1.run()
+
+# Lab 2 Page
+elif selection == "Lab 2":
+    lab2.run()
+
+# Lab 3 Page
+elif selection == "Lab 3":
+    lab3.run()
